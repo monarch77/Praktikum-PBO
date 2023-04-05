c955e73abb140870b5c0e604a224c475e118a17e
Aditya Suryandaru <91068712+monarch77@users.noreply.github.com>
2023-04-05 23:08:19 +0700

    Update AngkaSialException.java

--- Praktikum 6/AngkaSialException.java
+++ Praktikum 6/AngkaSialException.java
@@ -1,4 +1,9 @@
-
+/**
+* File		: AngkaSialException.java
+* Penulis	: Aditya Suryandaru - 24060121140105
+* Tanggal	: 29 Maret 2023
+* Deskripsi	: program penggunaan eksepsi menggunakan class library Java
+*/
 
 public class AngkaSialException extends Exception{
     public AngkaSialException(){
